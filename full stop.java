fd5de6ac9d0be3ffa272bd32eb9f4dd8de07f548
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-05 10:12:16 +0530

    Create full stop.java

---
+++ full stop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class Full stop
+{
+public statis void main(String[] args)
+{
+String name;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the string");
+name=sc.nextLine();
+System.out.println(name=".");
+}
+}
